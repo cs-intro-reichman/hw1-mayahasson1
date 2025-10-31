77aa3edad065c703d26493e3d8fc6efd0e3a950d
Maya Hasson <maya.hasson@post.runi.ac.il>
2025-10-31 23:04:32 +0200

    Completed Assignment

--- Bill3.java
+++ Bill3.java
@@ -4,9 +4,8 @@ public class Bill3 {
 	    String name1 = args[0];
 	    String name2 = args[1];
 		String name3 = args[2];
-		double sum1= Double.parseDouble(args[3]);
-		sum1=sum1/3;
-		double sum2=Math.ceil(sum1);
-		System.out.println(name3+", "+name2+", "+name1+", pay "+sum2+" shekels.");
+		int sum1= Integer.parseInt(args[3]);
+		double sum2=Math.ceil((double)sum1/3);
+		System.out.println("Dear "+name3+", "+name2+", and "+name1+": pay "+sum2+" Shekels each.");
 	}
 }
